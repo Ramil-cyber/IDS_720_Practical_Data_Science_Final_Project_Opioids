fc2c9e12755ccb11c6f8cd17133f48a992c70b80
Ramil Mammadov <ramil.mammadov2021@gmail.com>
2024-11-21 01:43:59 -0500

    update

--- 06_Tests/sample_1000_test.py
+++ 06_Tests/sample_1000_test.py
@@ -1,16 +1,6 @@
-# import pandas as pd
-# opioid_test = pd.read_csv(
-#     "https://github.com/MIDS-at-Duke/opioids-2024-optoid-drug-mortality-group5/raw/refs/heads/main/06_Tests/sample_data_1000_opoidshipment(prescription_b2b).csv"
-#     )
-# print(opioid_test)
-
 import pandas as pd
-
-# Define the URL for the CSV file
-url = "https://github.com/MIDS-at-Duke/opioids-2024-optoid-drug-mortality-group5/raw/refs/heads/main/06_Tests/sample_data_1000_opoidshipment%28prescription_b2b%29.csv"
-
-# Read the CSV file from the URL
-opioid_test = pd.read_csv(url)
-
-# Print the DataFrame
+opioid_test = pd.read_csv(
+    "sample_data_1000_opoidshipment(prescription_b2b).csv"
+    )
 print(opioid_test)
+
